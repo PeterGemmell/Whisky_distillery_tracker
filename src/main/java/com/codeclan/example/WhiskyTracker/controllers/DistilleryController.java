33103cda7b91796b0b8bfb7a9240c50fd2dd2841
Peter Gemmell <petergemmell65@hotmail.com>
2020-04-01 17:47:48 +0100

    whisky and distilleries tracker

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -0,0 +1,33 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/distilleries")
+public class DistilleryController {
+
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping()
+    public ResponseEntity findDistilleryByRegion(@RequestParam(name="region", required = false) String region){
+        if(region != null){
+            return new ResponseEntity(distilleryRepository.findDistilleryByRegion(region), HttpStatus.OK);
+        }
+        return new ResponseEntity(distilleryRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/age")
+    public ResponseEntity findDistilleriesWithinWhiskyAge12(@RequestParam(name = "age", required = false) int age){
+        return new ResponseEntity(distilleryRepository.findByWhiskiesAge(age), HttpStatus.OK);
+    }
+
+
+}
